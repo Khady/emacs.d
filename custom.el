8ec92f3f0477e42849faf19fa8798c4fa60825c8
Louis Roché <louis@louisroche.net>
2017-12-11 09:35:24 +0000

    create config with use-package and org-babel

---
+++ custom.el
@@ -0,0 +1,44 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-matching-paren t)
+ '(blink-matching-paren-dont-ignore-comments t)
+ '(blink-matching-paren-on-screen t)
+ '(company-quickhelp-delay 0)
+ '(company-tooltip-align-annotations t)
+ '(custom-safe-themes
+   (quote
+    ("a7e7804313dbf827a441c86a8109ef5b64b03011383322cbdbf646eb02692f76" default)))
+ '(doom-themes-enable-bold t)
+ '(doom-themes-enable-italic t)
+ '(git-gutter-fr:side (quote right-fringe))
+ '(helm-M-x-fuzzy-match t)
+ '(helm-buffers-fuzzy-matching t)
+ '(helm-completion-in-region-fuzzy-match t)
+ '(helm-ff-fuzzy-matching t)
+ '(helm-mode-fuzzy-match t)
+ '(helm-recentf-fuzzy-match t t)
+ '(js-indent-level 2)
+ '(json-reformat:indent-width 1)
+ '(linum-format " %2d")
+ '(package-selected-packages
+   (quote
+    (htmlize zenburn-theme caml tuareg scratch restclient vline vlf multiple-cursors expand-region magit iedit projectile-ripgrep helm-swoop helm-projectile helm git-gutter-fringe fish-mode fish-completion evil-matchit evil editorconfig autopair rainbow-delimiters yaml-mode puppet-mode protobuf-mode markdown-mode rust-mode json-mode web-mode indium tide company-quickhelp company ob-http org-plus-contrib exec-path-from-shell solarized-theme doom-themes diminish use-package)))
+ '(projectile-completion-system (quote helm))
+ '(projectile-enable-caching t)
+ '(projectile-switch-project-action (quote helm-projectile-find-file))
+ '(save-place t)
+ '(save-place-file "/home/louis/.emacs.d/.places")
+ '(save-place-mode t)
+ '(show-paren-delay 0)
+ '(show-paren-style (quote expression))
+ '(whitespace-line-column 160)
+ '(whitespace-style (quote (face empty tabs lines-tail trailing))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,40 @@
+(setq gc-cons-threshold 400000000)
+
+;;; Begin initialization
+;; Turn off mouse interface early in startup to avoid momentary display
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+;;; Set up package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(when (boundp 'package-pinned-packages)
+  (setq package-pinned-packages
+        '((org-plus-contrib . "org"))))
+(package-initialize)
+
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'diminish)
+  (package-install 'use-package)
+  )
+
+;; From use-package README
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(setq gc-cons-threshold 800000)
