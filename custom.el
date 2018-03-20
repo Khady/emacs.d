e80b120be12773843f84fdc8f9c21ab375d78c43
Louis Roché <louis.roche@ahrefs.com>
2018-03-20 13:46:18 +0000

    all the best

--- custom.el
+++ custom.el
@@ -21,22 +21,21 @@
  '(helm-completion-in-region-fuzzy-match t)
  '(helm-ff-fuzzy-matching t)
  '(helm-mode-fuzzy-match t)
- '(helm-recentf-fuzzy-match t t)
+ '(helm-recentf-fuzzy-match t)
  '(js-indent-level 2)
  '(json-reformat:indent-width 1)
  '(linum-format " %2d")
- '(merlin-command (quote opam))
- '(merlin-completion-with-doc t)
+ '(merlin-command (quote opam) t)
+ '(merlin-completion-with-doc t t)
  '(org-agenda-files
    (quote
     ("~/Code/github/emacs.d/config.org" "~/Notes/ahrefs.org" "~/Notes/beorg-local.org")))
  '(package-selected-packages
    (quote
-    (edit-indirect zerodark-theme monokai-theme reason-mode nginx-mode tree-mode json-navigator ahg monky browse-at-remote git-link git-gutter htmlize zenburn-theme caml tuareg scratch restclient vline vlf multiple-cursors expand-region magit iedit projectile-ripgrep helm-swoop helm-projectile helm git-gutter-fringe fish-mode fish-completion evil-matchit evil editorconfig autopair rainbow-delimiters yaml-mode puppet-mode protobuf-mode markdown-mode rust-mode json-mode web-mode indium tide company-quickhelp company ob-http org-plus-contrib exec-path-from-shell solarized-theme doom-themes diminish use-package)))
+    (aggressive-indent elisp-def edit-indirect zerodark-theme monokai-theme reason-mode nginx-mode tree-mode json-navigator ahg monky browse-at-remote git-link git-gutter htmlize zenburn-theme caml tuareg scratch restclient vline vlf multiple-cursors expand-region magit iedit projectile-ripgrep helm-swoop helm-projectile helm git-gutter-fringe fish-mode fish-completion evil-matchit evil editorconfig autopair rainbow-delimiters yaml-mode puppet-mode protobuf-mode markdown-mode rust-mode json-mode web-mode indium tide company-quickhelp company ob-http org-plus-contrib exec-path-from-shell solarized-theme doom-themes diminish use-package)))
  '(projectile-completion-system (quote helm))
  '(projectile-enable-caching t)
  '(projectile-switch-project-action (quote helm-projectile-find-file))
- '(save-place t)
  '(save-place-file "/home/louis/.emacs.d/.places")
  '(save-place-mode t)
  '(show-paren-delay 0)
