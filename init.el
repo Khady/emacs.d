9af3b2a18dd1c0bdf2ae7002d2be757f06111767
Louis Roché <louis@louisroche.net>
2019-03-03 16:00:32 +0000

    ocaml

--- init.el
+++ init.el
@@ -12,8 +12,8 @@
 
 ;;; Set up package
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (when (boundp 'package-pinned-packages)
   (setq package-pinned-packages
         '((org-plus-contrib . "org"))))
